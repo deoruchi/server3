a060074f329d888a0f2a8ab2f1a1d31e39324cd5
kurokurotho <deoruchi064@gmail.com>
2023-11-24 00:25:14 +0530

    Permission denied: nodemon fixing

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const cors = require("cors");
 const urlRouter1 = require("./Router/authRouter");
 
 const app = express();
+
+app.use(express.json());
 app.use(cors());
+app.use("/url", urlRouter1);
+
 mongoose
   .connect("mongodb://0.0.0.0:27017/Practice")
   .then(() => console.log("Connected"))
   .catch((error) => console.log(error.messege));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/url", urlRouter1);
 app.listen(PORT, () => {
   console.log("server is running");
 });
